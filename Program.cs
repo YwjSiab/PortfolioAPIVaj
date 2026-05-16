a5243aab4a57a3b472eb09a733801e4d7f179ac2
Ywj Siab Vaj <yvaj@students.ntc.edu>
2026-05-16 15:46:55 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:4200",
                 "https://ntc-portfolio-vaj.infinityfreeapp.com",
-                "http://localhost:5173"
+                "http://localhost:5173".
+                "http://localhost:8081"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
